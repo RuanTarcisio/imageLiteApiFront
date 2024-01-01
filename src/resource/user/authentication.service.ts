69fcfe95dae6a3fbc4e8116e4e4affb42f5b85b4
RuanTariciso <ruantariciso@localhost.localdomain>
2024-01-01 19:45:39 -0300

    login e autenticacao ok

--- src/resource/user/authentication.service.ts
+++ src/resource/user/authentication.service.ts
@@ -1,4 +1,5 @@
-import { AcessToken, Credentials, User, UserSessionToken } from "./users.resource";
+import { AccessToken, Credentials, User, UserSessionToken } from "./users.resource";
+import {jwtDecode} from 'jwt-decode'
 
 class AuthService{
     baseURL: string = "http://localhost:8080/v1/users";
@@ -15,10 +16,82 @@ class AuthService{
         });
 
         if(response.status == 401){
-            throw new Error("User or password aere incorrect!");
+            throw new Error("User or password are incorrect!");
         }
 
         return await response.json
     }  
+    async save(user: User) : Promise<void> {
+        const response = await fetch(this.baseURL, {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if(response.status == 409){
+            const responseError = await response.json();
+            throw new Error(responseError.error);
+        }
+    }
+
+    initSession(token: AccessToken){
+        if(token.accessToken){
+            const decodedToken: any = jwtDecode(token.accessToken);
+            
+            const userSessionToken: UserSessionToken = {
+                accessToken: token.accessToken,
+                email: decodedToken.sub,
+                name: decodedToken.name,
+                expiration: decodedToken.exp
+            }
+
+            this.setUserSession(userSessionToken);
+        }
+    }
+
+    setUserSession(userSessionToken: UserSessionToken){
+        try{
+            localStorage.setItem(AuthService.AUTH_PARAM, JSON.stringify(userSessionToken));
+        }catch(error){}
+    }
+
+    getUserSession() : UserSessionToken | null {
+        try{
+            const authString = localStorage.getItem(AuthService.AUTH_PARAM);
+            if(!authString){
+                return null;
+            }
+
+            const token: UserSessionToken = JSON.parse(authString);
+            return token;
+        }catch(error){
+            return null;
+        }
+    }
+
+    isSessionValid() : boolean {
+        const userSession: UserSessionToken | null = this.getUserSession();
+        if(!userSession){
+            return false;
+        }
+
+        const expiration: number | undefined = userSession.expiration;
+        if(expiration){
+            const expirationDateInMillis = expiration * 1000;
+            return new Date() < new Date(expirationDateInMillis);
+        }
+
+        return false;
+    }
+
+    invalidateSession(): void {
+        try{
+            localStorage.removeItem(AuthService.AUTH_PARAM);
+        }catch(error){}
+    }
+
 }
- export const useAuth = () => new AuthService();
+
+export const useAuth = () => new AuthService(); 
