c8480141e5589a95f17fe388ed88729501ac8b58
Ruan Tarcisio <ruantarciisio@gmail.com>
2025-03-13 15:31:59 -0300

    adicionado calendario

--- src/app/register/registerFormSchema.ts
+++ src/app/register/registerFormSchema.ts
@@ -1,15 +1,34 @@
 import * as Yup from 'yup';
 
-export interface RegisterForm {
+// Função para calcular a data máxima (10 anos atrás a partir de hoje)
+const getMaxBirthDate = () => {
+  const currentDate = new Date();
+  const maxBirthDate = new Date(
+    currentDate.getFullYear() - 6, // Subtrai 10 anos
+    currentDate.getMonth(),
+    currentDate.getDate()
+  );
+  return maxBirthDate;
+};
+
+export interface RegisterFormValues {
   name: string;
   email: string;
   password: string;
   passwordMatch: string;
+  birthDate: Date | null;
 }
 
-export const registerValidationScheme = Yup.object().shape({
-  name: Yup.string().required('Name is required!'),
-  email: Yup.string().trim().required('Email is required!').email('Invalid Email!'),
-  password: Yup.string().required('Password is required').min(8, 'Password must have at least 8 characters!'),
-  passwordMatch: Yup.string().oneOf([Yup.ref('password')], 'Password must match!'),
-});
+export const registerValidationScheme = Yup.object({
+  name: Yup.string().required('Nome é obrigatório'),
+  email: Yup.string().email('E-mail inválido').required('E-mail é obrigatório'),
+  password: Yup.string().required('Senha é obrigatória'),
+  passwordMatch: Yup.string()
+    .oneOf([Yup.ref('password')], 'As senhas não coincidem')
+    .required('Confirme a senha'),
+  birthDate: Yup.date()
+    .nullable()
+    .required('Data de nascimento é obrigatória')
+    .max(new Date(), 'A data não pode ser no futuro')
+    .max(getMaxBirthDate(), 'Idade mínima, 06 anos de idade'), 
+  });
